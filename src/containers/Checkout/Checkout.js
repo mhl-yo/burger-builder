0a678c402b9ce95aa66a6b486fc78899318e16d0
mhl-yo <myjubox@gmail.com>
2018-10-31 17:39:40 +0200

    Routing. Checout. CheckoutSummary

---
+++ src/containers/Checkout/Checkout.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+
+import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+
+class Checkout extends Component {
+    state = {
+        ingredients: {
+            salad: 1,
+            meat: 1,
+            cheese: 1,
+            bacon: 1
+        }
+    };
+
+    componentDidMount() {
+        const query = new URLSearchParams(this.props.location.search);
+        const ingredients = {};
+        for (let param of query.entries()) {
+            ingredients[param[0]] = +param[1];
+        }
+        this.setState({ingredients: ingredients});
+    }
+
+    CheckoutCancelled = () => {
+        this.props.history.goBack();
+    };
+
+    CheckoutContinued = () => {
+        this.props.history.replace('/checkout/contact-data');
+    };
+
+    render() {
+        return (
+            <div>
+                <CheckoutSummary
+                    ingredients={this.state.ingredients}
+                    checkoutCancelled={this.CheckoutCancelled}
+                    checkoutContinued={this.CheckoutContinued}
+                />
+            </div>
+        );
+    }
+}
+
+export default Checkout;
